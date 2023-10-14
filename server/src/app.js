a1a955a575c84fd1bb699dd886d63ab78b774e75
Administrator <p.z.palasti@gmail.com>
2023-10-14 18:30:40 +0200

    deploy dependevies

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express"); //minde express code-ot ide pakolunk h tiszta legyen a server file
 const cors = require("cors");
+const path= require('path')
 
 const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
@@ -10,6 +11,12 @@ app.use(
   })
 ); // CORS midelwear
 app.use(express.json()); //h ha a bejött adat = JSONnal...
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+
 app.use(planetsRouter); //akkor felhasználja a planetRouter => lásd "planet.router.js"
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+} )
 
 module.exports = app;
